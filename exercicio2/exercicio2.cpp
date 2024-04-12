d6e25304e63a57af53ac7b1bfa22ceacf188574c
Laura <133364744+laurajhagsuUGAVGHA@users.noreply.github.com>
2024-04-12 16:55:08 -0300

    .

---
+++ exercicio2/exercicio2.cpp
@@ -0,0 +1,63 @@
+// SEGUNDA LISTA 
+
+// PRIMEIRO EXERCICIO 
+
+#include <iostream>
+/*
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int unidade = 25;
+	float AteVinteCinco = 1.50;
+	float AcimaDeVinteCinco = 1.25;
+
+	if (unidade <= 25) {
+		int mult = unidade * AteVinteCinco;
+		cout << mult;
+	}
+
+	if (unidade >= 26) {
+		int mult = unidade * AcimaDeVinteCinco;
+		cout << mult;
+	}
+
+	return 0; 
+} */
+
+
+//SEGUNDO EXERCICIO 
+/*
+using namespace std;
+int main(int argc, char* argv[]) {
+	int numero = 10;
+
+
+	int(numero >= 10) {
+		int soma = numero + 10 * 2;
+		cout << soma; 
+	}
+
+	int(numero <= 10) {
+		int soma = numero + 9 * 3 – 8;
+		cout << soma;
+	}
+
+
+	return 0;
+} */
+
+//TERCEIRO EXERCICIO 
+
+/*int numero1 = 0;
+
+
+if (numero1 >= 0 ) {
+	cout << "positivo";
+}
+else if (numero1 <= 0) {
+	cout << "negativo";
+}
+
+return 0; */
+
+//QUARTO EXERCICIO
